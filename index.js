0b4b913532528a24f102ce0be1d34b7300d31158
bulbul <bulbulzetra93@gmail.com>
2023-06-09 18:27:42 +0600

    readme added

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json())
 app.use(cors())
 
+
 // MongoDB uri
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0ztfqf2.mongodb.net/?retryWrites=true&w=majority`;
 
